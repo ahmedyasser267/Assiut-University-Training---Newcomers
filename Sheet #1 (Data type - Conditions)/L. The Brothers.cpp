f0c871755f9b64489e5439159eb9f2b3ca3084a2
ahmed yasser mohamed <89811009+ahmedyasser267@users.noreply.github.com>
2022-12-24 16:36:46 +0200

    Create L. The Brothers.cpp

---
+++ Sheet #1 (Data type - Conditions)/L. The Brothers.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include<cstring>
+using namespace std;
+ 
+int main()
+{
+   string s1,s2,f1,f2;
+   cin>>s1>>f1;
+   cin>>s2>>f2;
+   if(f1==f2 )
+   {
+       cout<<"ARE Brothers";
+   }
+else{
+    cout<<"NOT";
+}
+}
