944f114a94c6b08e7b87ee447f61dc27c65409d2
ahmed yasser mohamed <89811009+ahmedyasser267@users.noreply.github.com>
2022-12-24 16:29:39 +0200

    Create B. Basic Data Types.cpp

---
+++ Sheet #1 (Data type - Conditions)/B. Basic Data Types.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int i ;
+    long long y ;
+    char n ;
+    float e ;
+    double z;
+    cin >> i >> y >> n >> e >> z;
+    cout << i <<"\n" << y << "\n" << n <<"\n" << e << "\n" << z;
+ 
+ 
+}
