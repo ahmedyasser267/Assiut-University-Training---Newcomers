eec5b8da692fa1ed0ae2fef7a4b41d8e4c90e9b4
ahmed yasser mohamed <89811009+ahmedyasser267@users.noreply.github.com>
2022-12-24 16:47:00 +0200

    Create Z. Hard Compare.cpp

---
+++ Sheet #1 (Data type - Conditions)/Z. Hard Compare.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include<bits/stdc++.h>
+ 
+using namespace std;
+ 
+int main()
+{
+    double a,b,c,d;
+    cin>>a>>b>>c>>d;
+    double res1=b*log(a);
+    double res2=d*log(c);
+    if(res1>res2)
+       cout<<"YES";
+    else
+        cout<<"NO";
+ 
+ 
+ 
+}
