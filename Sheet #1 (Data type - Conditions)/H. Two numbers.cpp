cafcde25bbb1f3c5e9b9915c38206a2a10eb525c
ahmed yasser mohamed <89811009+ahmedyasser267@users.noreply.github.com>
2022-12-24 16:33:48 +0200

    Create H. Two numbers.cpp

---
+++ Sheet #1 (Data type - Conditions)/H. Two numbers.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+	double a, b;
+	cin >> a >> b;	cout << "floor " << a << " / " << b <<" = "<< floor(a / b)<<"\n";
+	cout << "ceil " << a << " / " << b << " = " << ceil(a / b)<<"\n";
+	cout << "round " << a << " / " << b << " = " << round(a / b)<<"\n";
+ 
+}
