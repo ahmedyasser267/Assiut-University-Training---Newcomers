43db7647e0f26f2448be5878c96c7c4c886be800
ahmed yasser mohamed <89811009+ahmedyasser267@users.noreply.github.com>
2022-12-24 16:31:39 +0200

    Create E. Area of a Circle.cpp

---
+++ Sheet #1 (Data type - Conditions)/E. Area of a Circle.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include<iomanip>
+using namespace std;
+ 
+int main()
+{
+ 
+    cout<< fixed <<setprecision(9);
+    double r,c =3.141592653;
+cin>>r;
+cout<<(r*r)*c ;
+ 
+ 
+ 
+ 
+}
