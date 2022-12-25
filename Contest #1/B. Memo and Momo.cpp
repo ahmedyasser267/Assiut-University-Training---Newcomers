540d1da7346928d3f56b0dc4b94edc4534e46a61
ahmed yasser mohamed <89811009+ahmedyasser267@users.noreply.github.com>
2022-12-25 22:20:45 +0200

    Create B. Memo and Momo.cpp

---
+++ Contest #1/B. Memo and Momo.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  long long  a,b,k;
+   cin>>a>>b>>k;
+   if(a%k==0&&b%k!=0)
+    cout<<"Memo";
+   else if(a%k!=0&&b%k==0)
+    cout<<"Momo";
+    else if(a%k==0&&b%k==0)
+    cout<<"Both";
+    else
+        cout<<"No One";
+
+}
