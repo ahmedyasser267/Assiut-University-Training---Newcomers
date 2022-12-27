9cc3f9ea4cae934f28bc8c716cdcc7f9d71a4094
ahmed yasser mohamed <89811009+ahmedyasser267@users.noreply.github.com>
2022-12-28 00:40:11 +0200

    Create A. Create A New String.cpp

---
+++ Sheet #4 (Strings)/A. Create A New String.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<cmath>
+#include<algorithm> // for lexicographical_compare()
+using namespace std;
+
+int main()
+{
+	string x, y;
+	cin >> x >> y;
+	cout << x.size() << " " << y.size()<<endl;
+	cout << x << " " << y<<endl;
+}
