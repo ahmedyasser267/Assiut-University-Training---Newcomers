47c2334cec2ffb481d8ed577f88bb4cdfa38c1b7
ahmed yasser mohamed <89811009+ahmedyasser267@users.noreply.github.com>
2022-12-28 00:48:53 +0200

    Create H. N Times.cpp

---
+++ Sheet #5 (Functions)/H. N Times.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+void a(){
+	int arr[100];
+	int x;
+	cin >> x;
+	int n;
+	char s;
+	for (int i = 0;i < x;i++)
+	{
+		cin >> n >> s;
+		for (int j = 0;j < n;j++)
+		{
+			
+			cout << s<<" ";
+			
+		}
+		for (int j = 0;j < 1;j++)
+		{
+
+			cout << endl;
+				
+
+		}
+
+	}
+}
+int main()
+{
+	
+	a();
+	return 0;
+}
