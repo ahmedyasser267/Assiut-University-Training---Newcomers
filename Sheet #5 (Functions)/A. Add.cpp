a5d3277d94856225492aed3d3f99095e99898962
ahmed yasser mohamed <89811009+ahmedyasser267@users.noreply.github.com>
2022-12-28 00:45:51 +0200

    Create A. Add.cpp

---
+++ Sheet #5 (Functions)/A. Add.cpp
@@ -0,0 +1,18 @@
+ 
+#include <iostream>
+using namespace std;
+
+int ahmed(int x, int y)
+{
+	cin >> x >> y;
+	int r = x + y;
+	return r;
+	cout << r;
+}
+
+int main()
+{
+	
+	cout<<ahmed(2,5);
+}
+ 
