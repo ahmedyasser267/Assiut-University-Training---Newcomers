60ffd195512fc00db76aeff4a07efadc915e36b6
ahmed yasser mohamed <89811009+ahmedyasser267@users.noreply.github.com>
2022-12-28 00:47:29 +0200

    Create E. Swap.cpp

---
+++ Sheet #5 (Functions)/E. Swap.cpp
@@ -0,0 +1,22 @@
+ 
+#include <iostream>
+#include<cmath>
+#include<algorithm>
+
+using namespace std;
+
+int ahmed (int x,int y)
+
+{
+	cin >> x >> y;
+	cout << y << " " << x;
+	return x;
+}
+
+int main()
+{
+	 
+	ahmed(0,0);
+	
+}
+ 
